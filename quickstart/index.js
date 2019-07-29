5092948f444a87cb532e0e46a1c58874796a4c7b
lobrien <lobrien@knowing.net>
2019-07-29 13:32:35 -1000

    Using nconf, I think

--- quickstart/index.js
+++ quickstart/index.js
@@ -1,7 +1,13 @@
 #!/usr/bin/env/node
 
+const nconf = require('nconf')
+const config = nconf.file({ file: "config.json" });
+
+console.log(config.get("serviceName"));
+
 const run = async() => {
     console.log("Hello, Node from CLI");
+
 }
 
 run();
