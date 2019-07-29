9bad5248d5eb43065d7e10c1ac9f3a5fcebca3af
lobrien <lobrien@knowing.net>
2019-07-29 13:50:39 -1000

    Basic search config

--- quickstart/index.js
+++ quickstart/index.js
@@ -1,12 +1,22 @@
 #!/usr/bin/env/node
 
 const nconf = require('nconf')
-const config = nconf.file({ file: "config.json" });
 
-console.log(config.get("serviceName"));
+function getAzureConfiguration() {
+    const config = nconf.file({ file: 'azure_search_config.json' });
+    if (config.get('serviceName') == '[SEARCH_SERVICE_NAME' || config.get('apiKey') == '[SEARCH_SERVICE_API_KEY]') {
+        throw "You have not set the values in your azure_search_config.json file. Please change them to match your search service's values."
+    }
+    return config;
+}
 
 const run = async() => {
+    try {
+    const cfg = getAzureConfiguration();
     console.log("Hello, Node from CLI");
+    } catch (x) {
+        console.log(x);
+    }
 
 }
 
