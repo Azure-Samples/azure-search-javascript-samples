2d22f047de2b314fc058f8ba4926dac99ca82d81
Dina Berry <41597107+diberry@users.noreply.github.com>
2021-03-17 10:06:01 -0700

    Key leak fail

--- search-website/api/config.js
+++ search-website/api/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     SearchIndexName: process.env["SearchIndexName"] || "good-books",
-    SearchApiKey: process.env["SearchApiKey"] || "0DD93422395CE836EF53349EF9AAA700",
-    SearchServiceName: process.env["SearchServiceName"] || "diberry-cog-search-js",
+    SearchApiKey: process.env["SearchApiKey"] || "",
+    SearchServiceName: process.env["SearchServiceName"] || "",
     SearchFacets: process.env["SearchFacets"] || "authors*,language_code", 
 }
 if (!CONFIG.SearchIndexName || !CONFIG.SearchApiKey || !CONFIG.SearchServiceName) throw Error("./config.js::Cognitive Services key is missing");
