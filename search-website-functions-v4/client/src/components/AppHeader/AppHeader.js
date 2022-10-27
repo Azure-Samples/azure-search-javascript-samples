bf33bf2438589a42ffeb71052f748b9a78af57a3
Dina Berry <41597107+diberry@users.noreply.github.com>
2022-10-27 12:37:36 -0700

    fix with hash

--- search-website-functions-v4/client/src/components/AppHeader/AppHeader.js
+++ search-website-functions-v4/client/src/components/AppHeader/AppHeader.js
@@ -1,50 +1,35 @@
-import React from "react";
-import AppHeaderAuth from "../AppHeaderAuth/AppHeaderAuth";
-import { Link } from "react-router-dom";
+import React from 'react';
+import AppHeaderAuth from '../AppHeaderAuth/AppHeaderAuth';
 
-import logo from "../../images/microsoft_small.png";
+import logo from '../../images/microsoft_small.png';
 
-import "./AppHeader.css";
+import './AppHeader.css';
 
 export default function AppHeader() {
   return (
     <header className="header">
       <nav className="navbar navbar-expand-lg">
-        <Link className="navbar-brand" to={`/`}>
+        <a className="navbar-brand" href="/">
           <img src={logo} height="50" className="navbar-logo" alt="Microsoft" />
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
+        </a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <Link className="nav-link" href="/Search">
-                Search
-              </Link>
+              <a className="nav-link" href="/#/Search">Search</a>
             </li>
             <li className="nav-item">
-              <Link
-                className="nav-link"
-                href="https://azure.microsoft.com/services/search/"
-              >
-                Learn more
-              </Link>
+              <a className="nav-link" href="https://azure.microsoft.com/services/search/">Learn more</a>
             </li>
           </ul>
         </div>
 
         <AppHeaderAuth />
       </nav>
+      
     </header>
   );
-}
+};
--- search-website-functions-v4/client/src/components/Results/Result/Result.js
+++ search-website-functions-v4/client/src/components/Results/Result/Result.js
@@ -1,23 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
 
-import "./Result.css";
+import './Result.css';
 
 export default function Result(props) {
-  console.log(`result prop = ${JSON.stringify(props)}`);
-
-  return (
+    
+    console.log(`result prop = ${JSON.stringify(props)}`)
+    
+    return(
     <div className="card result">
-      <Link to={`/details/${props.document.id}`}>
-        <img
-          className="card-img-top"
-          src={props.document.image_url}
-          alt={props.document.original_title}
-        ></img>
-        <div className="card-body">
-          <h6 className="title-style">{props.document.original_title}</h6>
-        </div>
-      </Link>
+        <a href={`/#/details/${props.document.id}`}>
+            <img className="card-img-top" src={props.document.image_url} alt={props.document.original_title}></img>
+            <div className="card-body">
+                <h6 className="title-style">{props.document.original_title}</h6>
+            </div>
+        </a>
     </div>
-  );
+    );
 }
