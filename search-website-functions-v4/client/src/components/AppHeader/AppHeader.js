5c2763e302b04663b5d294a1d47ae652d66b4666
Dina Berry <41597107+diberry@users.noreply.github.com>
2022-10-27 12:18:12 -0700

    Change anchor tag to link tag

--- search-website-functions-v4/client/src/components/AppHeader/AppHeader.js
+++ search-website-functions-v4/client/src/components/AppHeader/AppHeader.js
@@ -1,35 +1,50 @@
-import React from 'react';
-import AppHeaderAuth from '../AppHeaderAuth/AppHeaderAuth';
+import React from "react";
+import AppHeaderAuth from "../AppHeaderAuth/AppHeaderAuth";
+import { Link } from "react-router-dom";
 
-import logo from '../../images/microsoft_small.png';
+import logo from "../../images/microsoft_small.png";
 
-import './AppHeader.css';
+import "./AppHeader.css";
 
 export default function AppHeader() {
   return (
     <header className="header">
       <nav className="navbar navbar-expand-lg">
-        <a className="navbar-brand" href="/">
+        <Link className="navbar-brand" to={`/`}>
           <img src={logo} height="50" className="navbar-logo" alt="Microsoft" />
-        </a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <a className="nav-link" href="/Search">Search</a>
+              <Link className="nav-link" href="/Search">
+                Search
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="https://azure.microsoft.com/services/search/">Learn more</a>
+              <Link
+                className="nav-link"
+                href="https://azure.microsoft.com/services/search/"
+              >
+                Learn more
+              </Link>
             </li>
           </ul>
         </div>
 
         <AppHeaderAuth />
       </nav>
-      
     </header>
   );
-};
+}
