745cb2dd33de5a87729365cb50bcceba150bf60f
Dina Berry <dinaberry@outlook.com>
2023-08-19 06:38:13 -0700

    horizontal rule in footer

--- search-website-functions-v4/client-v4-no-bootstrap/src/components/AppFooter.js
+++ search-website-functions-v4/client-v4-no-bootstrap/src/components/AppFooter.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import Box from "@mui/material/Box";
 import { Typography } from "@mui/material";
+import Divider from '@mui/material/Divider';
+import styled from "@emotion/styled";
+
+const StyledBox = styled(Box)`
+  margin-top: 1rem;
+  padding: 1rem;
+  color: #666;
+  text-align: center;
+`;
+
+const StyledDivider = styled(Divider)`
+color: #666;
+margin: 1rem 0;
+border-top: 1px solid #666;
+opacity: 0.25;
+`;
+
+const StyledTypography = styled(Typography)`
+font-size: 0.85em;
+`;
 
 export default function AppFooter() {
   return (
-    <Box sx={{
-      marginLeft: "16px",
-    }}>
-      <Typography >&copy; {new Date().getFullYear()} Microsoft</Typography>
-    </Box>
+    <StyledBox>
+      <StyledDivider />
+      <StyledTypography >&copy; {new Date().getFullYear()} Microsoft</StyledTypography>
+    </StyledBox>
   );
 }
