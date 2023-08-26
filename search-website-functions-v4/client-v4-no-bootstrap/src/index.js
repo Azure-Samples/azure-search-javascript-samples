0e6adb82bd99d29414637931b80447d7f5f2e3d6
Dina Berry <dinaberry@outlook.com>
2023-08-26 06:17:25 -0700

    fix body

--- search-website-functions-v4/client-v4-no-bootstrap/src/index.js
+++ search-website-functions-v4/client-v4-no-bootstrap/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
