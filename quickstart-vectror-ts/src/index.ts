d2447a2ab708d3a6e6d5187ae45fc3cf8d0fc7a9
Dina Berry <diberry@microsoft.com>
2025-06-23 15:40:21 -0700

    cp: index create/delete

--- quickstart-vectror-ts/src/index.ts
+++ quickstart-vectror-ts/src/index.ts
@@ -1,13 +1,13 @@
 import { DefaultAzureCredential } from "@azure/identity";
 import * as dotenv from "dotenv";
 import * as process from "process";
-import { 
-    SearchIndexClient, 
+import {
+    SearchIndexClient,
     SearchIndex,
-    SearchClient
+    SearchClient,
+    SearchFieldDataType 
 } from "@azure/search-documents";
 import { DOCUMENTS } from "./documents.js";
-import { SCHEMA } from "./schema.js";
 import { QUERY_VECTOR } from "./queryVector.js";
 import * as VectorSearch from './search.js';
 
@@ -15,21 +15,184 @@ const searchEndpoint = process.env.AZURE_SEARCH_ENDPOINT!;
 const credential = new DefaultAzureCredential();
 const indexName = process.env.AZURE_SEARCH_INDEX || "vector-search-quickstart";
 
-const indexDefinition = SCHEMA;
-
 async function createIndex(): Promise<SearchIndexClient> {
 
     const indexClient = new SearchIndexClient(searchEndpoint, credential);
-    
+
     console.log('Creating index...');
-    const result = await indexClient.createOrUpdateIndex(indexDefinition as any);
+
+    const indexName = process.env.AZURE_SEARCH_INDEX || "vector-search-quickstart";
+
+    const indexDefinition: SearchIndex = {
+        name: indexName,
+        fields: [
+            {
+                name: "HotelId",
+                type: "Edm.String",
+                key: true,
+                filterable: true
+            },
+            {
+                name: "HotelName",
+                type: "Edm.String",
+                searchable: true,
+                sortable: true
+            },
+            {
+                name: "Description",
+                type: "Edm.String",
+                searchable: true
+            },
+            {
+                name: "DescriptionVector",
+                type: "Collection(Edm.Single)",
+                searchable: true,
+                vectorSearchDimensions: 1536,
+                vectorSearchProfileName: "my-vector-profile"
+            },
+            {
+                name: "Category",
+                type: "Edm.String",
+                searchable: true,
+                sortable: true,
+                filterable: true,
+                facetable: true
+            },
+            {
+                name: "Tags",
+                type: "Collection(Edm.String)",
+                searchable: true,
+                filterable: true,
+                facetable: true
+            },
+            {
+                name: "ParkingIncluded",
+                type: "Edm.Boolean",
+                filterable: true,
+                sortable: true,
+                facetable: true
+            },
+            {
+                name: "LastRenovationDate",
+                type: "Edm.DateTimeOffset",
+                filterable: true,
+                sortable: true,
+                facetable: true
+            },
+            {
+                name: "Rating",
+                type: "Edm.Double",
+                filterable: true,
+                sortable: true,
+                facetable: true
+            },
+            {
+                name: "Address",
+                type: "Edm.ComplexType",
+                fields: [
+                    {
+                        name: "StreetAddress",
+                        type: "Edm.String",
+                        searchable: true
+                    },
+                    {
+                        name: "City",
+                        type: "Edm.String",
+                        searchable: true,
+                        filterable: true,
+                        sortable: true,
+                        facetable: true
+                    },
+                    {
+                        name: "StateProvince",
+                        type: "Edm.String",
+                        searchable: true,
+                        filterable: true,
+                        sortable: true,
+                        facetable: true
+                    },
+                    {
+                        name: "PostalCode",
+                        type: "Edm.String",
+                        searchable: true,
+                        filterable: true,
+                        sortable: true,
+                        facetable: true
+                    },
+                    {
+                        name: "Country",
+                        type: "Edm.String",
+                        searchable: true,
+                        filterable: true,
+                        sortable: true,
+                        facetable: true
+                    }
+                ]
+            },
+            {
+                name: "Location",
+                type: "Edm.GeographyPoint",
+                filterable: true,
+                sortable: true
+            }
+        ],
+        vectorSearch: {
+            algorithms: [
+                {
+                    name: "my-hnsw-vector-config-1",
+                    kind: "hnsw"
+                },
+                {
+                    name: "my-eknn-vector-config",
+                    kind: "exhaustiveKnn"
+                }
+            ],
+            profiles: [
+                {
+                    name: "my-vector-profile",
+                    algorithmConfigurationName: "my-hnsw-vector-config-1"
+                }
+            ]
+        },
+        semanticSearch: {
+            configurations: [
+                {
+                    name: "my-semantic-config",
+                    prioritizedFields: {
+                        titleField: {
+                            name: "HotelName"
+                        },
+                        contentFields: [
+                            {
+                                name: "Description"
+                            }
+                        ],
+                        keywordsFields: [
+                            {
+                                name: "Category"
+                            }
+                        ]
+                    }
+                }
+            ]
+        },
+        suggesters: [
+            {
+                name: "sg",
+                sourceFields: ["Tags", "Address/City", "Address/Country"],
+                searchMode: "analyzingInfixMatching"
+            }
+        ]
+    };
+
+    const result = await indexClient.createOrUpdateIndex(indexDefinition);
     console.log(`${result.name} created`);
 
     return indexClient;
 }
 async function deleteIndex(searchIndexClient: SearchIndexClient): Promise<void> {
     const indexClient = new SearchIndexClient(searchEndpoint, credential);
-    
+
     try {
         console.log("Deleting index...");
         await searchIndexClient.deleteIndex(indexName);
@@ -40,7 +203,7 @@ async function deleteIndex(searchIndexClient: SearchIndexClient): Promise<void> 
 }
 async function uploadDocuments(): Promise<void> {
     const searchClient = new SearchClient(searchEndpoint, indexName, credential);
-    
+
     try {
         console.log("Uploading documents...");
         const result = await searchClient.uploadDocuments(DOCUMENTS);
