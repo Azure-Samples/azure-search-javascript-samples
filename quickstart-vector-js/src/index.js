afa12949f1a29f0969c296fab2983fd131a4bd42
Dina Berry (MSFT) <diberry@microsoft.com>
2025-06-26 20:23:45 +0000

    cp

---
+++ quickstart-vector-js/src/index.js
@@ -0,0 +1,34 @@
+import {
+    createIndex,
+    uploadDocuments,
+    deleteIndex
+} from './manageIndex.js';
+import {
+    singleVectorSearch,
+    singleVectorSearchWithFilter,
+    vectorQueryWithGeoFilter,
+    hybridSearch,
+    semanticHybridSearch,
+} from './search.js';
+
+async function main() {
+    try {
+
+        //const searchIndexClient = await createIndex();
+        //await uploadDocuments();
+
+        await singleVectorSearch();
+        await singleVectorSearchWithFilter();
+        await vectorQueryWithGeoFilter();
+        await hybridSearch();
+        await semanticHybridSearch();
+
+        //await deleteIndex(searchIndexClient);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+main().catch((error) => {
+    console.error("An error occurred in the main function:", error);
+});
